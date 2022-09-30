400b7f1a697c54d903ca1ba6d56f178ab06e400b
Stefan Lica <Stefan.Lica@Dell.com>
2022-09-30 13:21:09 +0300

    Add project files.

---
+++ ProiectFinal/Controllers/AuthController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using ProiectFinal.Controllers.Services.UserService;
+using ProiectFinal.Data;
+using ProiectFinal.DTOs;
+using ProiectFinal.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace ProiectFinal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly DataContext _context;
+        private readonly IUserService _userService;
+        public string JwtToken = string.Empty;
+
+        public AuthController(IConfiguration configuration, DataContext context, IUserService userService)
+        {
+            _configuration = configuration;
+            _context = context;
+            _userService = userService;
+        }
+
+        [HttpGet("GetMe")]
+        [Authorize(Roles = "Admin,User")]
+        public async Task<ActionResult<Account>> GetMe()
+        {
+            string? userId = _userService.GetMyId();
+            string? userName = _userService.GetMyName();
+            string? userRole = _userService.GetMyRole();
+            if(userId == null)
+                return BadRequest(JsonConvert.SerializeObject(new { message = "Not logged in" }));
+
+            var account = await _context.Accounts.FindAsync(int.Parse(userId));
+            if(account == null)
+                return BadRequest(JsonConvert.SerializeObject(new { message = "User does not exist." }));
+
+            return account;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<Account>> Register(RegisterDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            if (!await VerifyEmail(request.Email))
+            {
+                return BadRequest(JsonConvert.SerializeObject(new { message = "User already exists." }));
+            }
+
+            if(request.FirstName == null || request.LastName == null ||
+                request.Email == null || request.Password == null ||
+                request.DateOfBirth == null || request.Phone == null ||
+                request.Address == null)
+            {
+                return BadRequest(JsonConvert.SerializeObject(new { message = "Invalid input." }));
+            }
+
+            var account = new Account
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                DateOfBirth = request.DateOfBirth,
+                Phone = request.Phone,
+                Address = request.Address,
+                Funds = 1000,
+                Admin = request.Admin
+            };
+
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+
+            return Ok(account);
+        }
+
+        private async Task<bool> VerifyEmail(string email)
+        {
+            var account = await _context.Accounts.FirstOrDefaultAsync(x => x.Email == email);
+            return account == null;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginDto request)
+        {
+            var account = await _context.Accounts.FirstOrDefaultAsync(x => x.Email == request.Email);
+
+            if (account == null)
+            {
+                return BadRequest(JsonConvert.SerializeObject(new { message = "User does not exist." }));
+            }
+
+            if (account.Email != request.Email)
+            {
+                return BadRequest(JsonConvert.SerializeObject(new { message = "User not found." }));
+            }
+
+            if (!VerifyPasswordHash(request.Password, account.PasswordHash, account.PasswordSalt))
+            {
+                return BadRequest(JsonConvert.SerializeObject(new { message = "Wrong password." }));
+            }
+
+            JwtToken = CreateToken(account);
+            var json = JsonConvert.SerializeObject(new { token = JwtToken });
+
+            return Ok(json);
+        }
+
+        private string CreateToken(Account account)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, account.Id.ToString()),
+                new Claim(ClaimTypes.Email, account.Email),
+                new Claim(ClaimTypes.Role, account.Admin == 1 ? "Admin" : "User")
+            };
+
+            SymmetricSecurityKey? key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            SigningCredentials? creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            JwtSecurityToken? token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            string? jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (HMACSHA512? hmac = new HMACSHA512(passwordSalt))
+            {
+                byte[]? computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (HMACSHA512? hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
